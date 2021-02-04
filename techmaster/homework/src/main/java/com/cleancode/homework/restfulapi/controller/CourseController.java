42f9cdaae09d8e824dbc0a2bfb4d57947677759a
longdd90jp <62064330+longdd90jp@users.noreply.github.com>
2021-02-04 18:09:26 +0700

    update

--- techmaster/homework/src/main/java/com/cleancode/homework/restfulapi/controller/CourseController.java
+++ techmaster/homework/src/main/java/com/cleancode/homework/restfulapi/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.cleancode.homework.restfulapi.controller;
 import com.cleancode.homework.restfulapi.dto.response.SearchCourseResDto;
 import com.cleancode.homework.restfulapi.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +19,12 @@ public class CourseController {
     private CourseService courseService;
 
     @GetMapping("/api/v1/courses")
+    @Cacheable("courses")
     public ResponseEntity<List<SearchCourseResDto>> createUser(
             @RequestParam(required = false) String keyword,
             @RequestParam(required = false) String sortBy) {
         List<SearchCourseResDto> response  = courseService.searchCourses(keyword, sortBy);
-        return new ResponseEntity<List<SearchCourseResDto>>(response, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
