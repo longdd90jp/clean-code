380d95d2d5d06982043986efd01e5f60d38644cc
longdd90jp <62064330+longdd90jp@users.noreply.github.com>
2021-02-04 07:31:55 +0700

    add

---
+++ techmaster/homework/src/main/java/com/cleancode/homework/phan2/bai5/model/Comment.java
@@ -0,0 +1,11 @@
+package com.cleancode.homework.phan2.bai5.model;
+
+public class Comment {
+
+    private long id;
+    private String title;
+    private Post post; //Bài viết mà comment gắn vào
+    private User author; //Tác giả của comment
+    //---- Bổ xung method vào đây. Nhiệm vụ của các bạn đó !
+
+}
---
+++ techmaster/homework/src/main/java/com/cleancode/homework/phan2/bai5/model/Post.java
@@ -0,0 +1,14 @@
+package com.cleancode.homework.phan2.bai5.model;
+
+import java.util.ArrayList;
+
+public class Post {
+
+    private long id;
+    private String title;
+    private String content;
+    private User author; //Tác giả của bài viết
+    private ArrayList<Comment> comments; //Một post chứa nhiều Comment
+    //---- Bổ xung method vào đây. Nhiệm vụ của các bạn đó !
+
+}
